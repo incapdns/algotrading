4a4691c1cf111a41fa32a2896a0b571005410de1
Lucas P <40347728+incapdns@users.noreply.github.com>
2026-01-20 08:56:35 -0300

    Update mod.rs

--- src/filters/mod.rs
+++ src/filters/mod.rs
@@ -1,3 +1,3 @@
 mod kalman;
 
-pub use kalman::{KalmanFilter1D, KalmanFilterND, KalmanUpdate, KalmanFilter2D, KalmanUpdateND, CovarianceHealth};
+pub use kalman::{KalmanFilter1D, KalmanFilterND, KalmanUpdate, KalmanFilter2D, KalmanUpdateND, CovarianceHealth, PVKalmanConfig, PVKalmanResult, PositionVelocityKalman};
